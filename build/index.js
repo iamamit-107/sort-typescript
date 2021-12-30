9f9b262535f94a94c3772b67515907d2155f6eae
Ahmed Faisal Amit <ahmed.amit107@gmail.com>
2021-12-30 23:09:34 +0600

    reformatting

--- build/index.js
+++ build/index.js
@@ -1,28 +1,8 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    //   bubble sort
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                //   Only work for array
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const temp = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = temp;
-                    }
-                }
-                // only work for string
-                if (typeof this.collection === "string") {
-                }
-            }
-        }
-    }
-}
-const sorter = new Sorter([10, 5, 18, -3]);
+Object.defineProperty(exports, "__esModule", { value: true });
+const numberCollection_1 = require("./numberCollection");
+const sorter_1 = require("./sorter");
+const numberCollection = new numberCollection_1.NumberCollection([10, 5, 18, -3]);
+const sorter = new sorter_1.Sorter(numberCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numberCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,7 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { NumberCollection } from "./numberCollection";
+import { Sorter } from "./sorter";
 
-  //   bubble sort
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        //   Only work for array
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const temp = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = temp;
-          }
-        }
-
-        // only work for string
-        if (typeof this.collection === "string") {
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 5, 18, -3]);
+const numberCollection = new NumberCollection([10, 5, 18, -3]);
+const sorter = new Sorter(numberCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numberCollection.data);
