0472c0c336d43ba8a0c09c8368e09596a98aad0f
Ahmed Faisal Amit <ahmed.amit107@gmail.com>
2021-12-30 23:22:55 +0600

    add interface in sorter class

--- build/sorter.js
+++ build/sorter.js
@@ -7,7 +7,7 @@ class Sorter {
     }
     //   bubble sort
     sort() {
-        const length = this.collection.length;
+        const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
                 if (this.collection.compare(j, j + 1)) {
--- src/sorter.ts
+++ src/sorter.ts
@@ -1,11 +1,15 @@
-import { NumberCollection } from "./numberCollection";
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+}
 
 export class Sorter {
-  constructor(public collection: NumberCollection) {}
+  constructor(public collection: Sortable) {}
 
   //   bubble sort
   sort(): void {
-    const length = this.collection.length;
+    const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
